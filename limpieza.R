ac2025ab5470e8950a31b480c68c80fc77f7bdcc
José Carlos Quintero Cedeño <jose.quinterocedeno@ucr.ac.cr>
2024-10-30 17:25:39 -0600

    feat: subir datos y agregar archivo de limpieza de datos

---
+++ limpieza.R
@@ -0,0 +1,11 @@
+library(readr)
+library(tidyverse)
+library(janitor)
+
+curvas_cero_cupon <- read_delim(
+  "datos/curvas-cero-cupon-csv.csv",
+  delim = ";",          # Especifica el delimitador como punto y coma
+  locale = locale(decimal_mark = ",")  # Define la coma como separador decimal
+)
+
+
