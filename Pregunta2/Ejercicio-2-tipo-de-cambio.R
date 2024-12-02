7aaa6a047cf129275f2eb5d88751af55ff298a96
Diego Vega Víquez <DIEGO.VEGAVIQUEZ@ucr.ac.cr>
2024-12-02 13:15:53 -0600

    feat: agrega la tabla a la parte de José

--- Pregunta2/Ejercicio-2-tipo-de-cambio.R
+++ Pregunta2/Ejercicio-2-tipo-de-cambio.R
@@ -1,7 +1,29 @@
+# Cargar paquetes
+library(readxl)
+library(lubridate)
+library(tidyverse)
+library(scales)
+library(xtable)
+
 #Ejercicio 2
 
-#Agregar las curvas bono cero cupon
+proyección <- read_excel("Pregunta2/pregunta2_Proyección.xlsx") %>% as.data.frame()
 
-curvas_cero_cupon <- curvas_cero_cupon %>% 
-  mutate(curva_bono_cero_cupon_bccr_colones = 1 / (1 + curva_cero_cupon_bccr_colones / 100) ^ (vencimiento_anos)) %>% 
-  mutate(curva_bono_cero_cupon_fed_dolares = 1 / (1 + curva_cero_cupon_fed_dolares / 100) ^ (vencimiento_anos))
+proyección$Fecha <- proyección$Fecha %>% as.Date() %>% format("%d/%m/%Y") %>% as.character()
+proyección$`Predicción tipo de cambio` <- proyección$`Predicción tipo de cambio` %>% as.numeric()
+
+proyección <- proyección %>%
+  mutate(across(where(is.numeric), 
+                ~ label_dollar(prefix = "₡", 
+                               big.mark = ",", 
+                               decimal.mark = ".",
+                               accuracy = 0.01)(.)))
+
+print(xtable(proyección), 
+      include.rownames = FALSE,  # No incluir los nombres de las filas
+      tabular.environment = "tabular",  # Usar 'tabular' en lugar de 'longtable'
+      floating = TRUE,  # Permitir que la tabla flote
+      hline.after = c(-1, 0, nrow(proyección)),  # Hacer líneas horizontales
+      booktabs = TRUE,  # Usar líneas de estilo 'booktabs'
+      title = "Proyección del Tipo de Cambio")  # Título de la tabla
+
